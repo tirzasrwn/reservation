817b1edc3c2c8aca986a1754b619c05a0e67e73a
tirzasrwn <tirzasrwn@gmail.com>
2022-03-03 13:29:14 +0800

    feat: Add server-side form validation II.

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -65,7 +65,30 @@ func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
 
 // PostReservation handles the posting of a reservation form.
 func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
-	log.Println("Posted")
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	reservation := models.Reservation{
+		FirstName: r.Form.Get("first_name"),
+		LastName:  r.Form.Get("last_name"),
+		Email:     r.Form.Get("email"),
+		Phone:     r.Form.Get("phone"),
+	}
+	form := forms.New(r.PostForm)
+	form.Has("first_name", r)
+	if !form.Valid() {
+		data := make(map[string]interface{})
+		data["reservation"] = reservation
+		stringMap := make(map[string]string)
+		render.RenderTemplate(w, r, "make-reservation.page.html", &models.TemplateData{
+			StringMap: stringMap,
+			Form:      form,
+			Data:      data,
+		})
+		return
+	}
 }
 
 // Generals renders the room page.
