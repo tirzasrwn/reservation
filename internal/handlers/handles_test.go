e5935ca2e1fc8fdef77e36b2c59f15f9ae76ba9c
tirzasrwn <tirzasrwn@gmail.com>
2022-05-20 10:34:54 +0800

    feat: Writing test for GET handlers.

---
+++ internal/handlers/handles_test.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type postData struct {
+	key   string
+	value string
+}
+
+var theTest = []struct {
+	name               string
+	url                string
+	method             string
+	params             []postData
+	expectedStatusCode int
+}{
+	{"home", "/", "GET", []postData{}, http.StatusOK},
+}
+
+func TestHandler(t *testing.T) {
+	routes := getRoutes()
+	ts := httptest.NewTLSServer(routes)
+	defer ts.Close()
+
+	for _, e := range theTest {
+		if e.method == "GET" {
+			resp, err := ts.Client().Get(ts.URL + e.url)
+			if err != nil {
+				t.Log(err)
+				t.Fatal(err)
+			}
+			if resp.StatusCode != e.expectedStatusCode {
+				t.Errorf("For %s, expected %d but got %d.", e.name, e.expectedStatusCode, resp.StatusCode)
+			}
+		} else {
+
+		}
+	}
+}
