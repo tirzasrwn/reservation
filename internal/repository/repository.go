66676de5b752cc3336e705a9a92895b3d7e17344
tirzasrwn <tirzasrwn@gmail.com>
2022-06-09 10:30:17 +0000

    feat: Inserting room restriction table.

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -4,5 +4,6 @@ import "github.com/tirzasrwn/reservation/internal/models"
 
 type DatabaseRepo interface {
 	AllUsers() bool
-	InsertReservation(res models.Reservation) error
+	InsertReservation(res models.Reservation) (int, error)
+	InsertRoomRestriction(r models.RoomRestriction) error
 }
