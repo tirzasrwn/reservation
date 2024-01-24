6482920cad8f70cfefd6058ad7a501d0e26fb290
tirzasrwn <tirzasrwn@gmail.com>
2024-01-24 20:39:56 +0700

    feat: Editing a reservation.

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -324,7 +324,7 @@ func (m *postgresDBRepo) GetReservationByID(id int) (models.Reservation, error) 
 func (m *postgresDBRepo) UpdateReservation(reservation models.Reservation) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
-	query := `update users set first_name = $1, last_name = $2, email = $3, phone = $4, updated_at = $5
+	query := `update reservations set first_name = $1, last_name = $2, email = $3, phone = $4, updated_at = $5
   where id = $6`
 	_, err := m.DB.ExecContext(ctx, query,
 		&reservation.FirstName,
